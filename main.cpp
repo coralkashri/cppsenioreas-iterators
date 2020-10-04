dbbc4ab920656028b03749b23e57f1d1c473e808
Korel Kashri <korelkashri@gmail.com>
2020-10-04 04:44:31 +0300

    Custom iterators example. Add examples for custom containers & collections.

--- main.cpp
+++ main.cpp
@@ -16,8 +16,9 @@
 #include <set>
 #include <thread>
 #include <mutex>
+#include <functional>
 
-#define ARTICLE_PART 2
+#define ARTICLE_PART 3
 
 #if ARTICLE_PART == 1
 #define USE_ITERATORS true
@@ -65,7 +66,6 @@ void print_container(const Iterator &first, const Iterator &last) {
 #endif
 
 #if ARTICLE_PART == 2
-
 class my_system {
 public:
     explicit my_system(int val) {
@@ -109,6 +109,189 @@ private:
 };
 #endif
 
+#if ARTICLE_PART == 3
+
+/*
+ * Concepts Declarations
+ */
+
+template <template <typename ...> typename Container, typename T>
+concept IteratorsFriendlyContainerType = requires (Container<T> container) {
+    typename decltype(container)::pointer;
+    typename decltype(container)::const_pointer;
+    typename decltype(container)::reference;
+    typename decltype(container)::const_reference;
+    typename decltype(container)::iterator;
+    typename decltype(container)::const_iterator;
+    typename decltype(container)::reverse_iterator;
+    typename decltype(container)::const_reverse_iterator;
+    typename decltype(container)::size_type;
+};
+
+template <typename T>
+concept HasMul = requires() {
+    { T() * T() };
+};
+
+template <typename T>
+concept IteratorTag = std::is_base_of_v<std::input_iterator_tag, T> || std::is_base_of_v<std::output_iterator_tag, T>;
+
+/*
+ * Base iterators structures
+ */
+
+template <IteratorTag ItTag, typename T>
+struct base_legacy_iterator {
+    typedef ItTag	                        iterator_category;
+    typedef T		                        value_type;
+    typedef T*                              pointer;
+    typedef T&	                            reference;
+    typedef std::iter_difference_t<pointer> difference_type;
+    // Iterator Concept: if (ItTag == random_access_iterator_tag) std::contiguous_iterator_tag; else ItTag;
+    using iterator_concept = std::conditional<std::is_same_v<ItTag, std::random_access_iterator_tag>, std::contiguous_iterator_tag, ItTag>;
+};
+
+template <template <typename ...> typename Container, typename T>
+requires IteratorsFriendlyContainerType<Container, T>
+struct use_collection_iterator {
+    typedef typename Container<T>::pointer pointer;
+    typedef typename Container<T>::const_pointer const_pointer;
+    typedef typename Container<T>::reference reference;
+    typedef typename Container<T>::const_reference const_reference;
+    typedef typename Container<T>::iterator iterator;
+    typedef typename Container<T>::const_iterator const_iterator;
+    typedef typename Container<T>::reverse_iterator reverse_iterator;
+    typedef typename Container<T>::const_reverse_iterator const_reverse_iterator;
+    typedef typename Container<T>::size_type size_type;
+};
+
+template <template <typename ...> typename IteratorType, typename Item>
+requires (!IteratorsFriendlyContainerType<IteratorType, Item>)
+struct use_collection_custom_iterator {
+    typedef IteratorType<Item> iterator;
+    typedef IteratorType<const Item> const_iterator;
+    typedef std::reverse_iterator<iterator> reverse_iterator;
+    typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+    typedef typename iterator::pointer pointer;
+    typedef typename const_iterator::pointer const_pointer;
+    typedef typename iterator::reference reference;
+    typedef typename const_iterator::reference const_reference;
+    typedef size_t size_type;
+};
+
+/*
+ * Custom Iterators
+ */
+
+template <typename T>
+class my_iterator : public base_legacy_iterator<std::random_access_iterator_tag, T> {
+public:
+    // A convenient access to inherited type members
+    using base_it = base_legacy_iterator<std::random_access_iterator_tag, T>;
+
+    // Ctors & Assign Operators & Dtor
+    explicit my_iterator<T>(T* data = nullptr) : _data(data) {}
+    my_iterator<T>(const my_iterator<T>&) = default;
+    my_iterator<T>(my_iterator<T>&&) noexcept = default;
+    my_iterator<T>& operator=(const my_iterator<T>&) = default;
+    my_iterator<T>& operator=(my_iterator<T>&&) noexcept = default;
+    my_iterator<T>& operator=(T* data) { _data = data; return *this; };
+    ~my_iterator<T>() = default;
+
+    // Is null
+    explicit operator bool() const { return _data; }
+
+    /* Required Operators For Iterator Category */
+
+    // Required Compare Operators
+    bool operator==(const my_iterator<T>& ref) const { return _data == ref._data; }
+    bool operator!=(const my_iterator<T>& ref) const { return !(*this == ref); }
+    typename base_it::difference_type operator-(const my_iterator<T>& ref) { return std::distance(ref._data, _data); }
+
+    // Required Action Operators
+    my_iterator<T>& operator+=(const typename base_it::difference_type& diff) { _data += diff; return *this; }
+    my_iterator<T>& operator-=(const typename base_it::difference_type& diff) { _data -= diff; return *this; }
+    my_iterator<T>& operator++() { ++_data; return *this; }
+    my_iterator<T> operator++(int) { auto temp = *this; ++_data; return temp; }
+    my_iterator<T>& operator--() { --_data; return *this; }
+    my_iterator<T> operator--(int) { auto temp = *this; --_data; return temp; }
+    my_iterator<T> operator+(const typename base_it::difference_type& diff) { auto temp = *this; temp._data += diff; return temp; }
+    my_iterator<T> operator-(const typename base_it::difference_type& diff) { auto temp = *this; temp._data -= diff; return temp; }
+
+    // Required Access Operators
+    T& operator*() { return *_data; }
+    const T& operator*() const  { return *_data; }
+    T* operator->() const  { return _data; }
+
+private:
+    T* _data;
+};
+
+/*
+ * Items & Collections
+ */
+
+template <HasMul T>
+class my_item {
+public:
+    my_item(const T &a, const T &b) : value_a(a), value_b (b) {}
+
+    T item() {
+        return value_a * value_b;
+    }
+
+    void set_a(T a) {
+        value_a = a;
+    }
+
+    void set_b(T b) {
+        value_b = b;
+    }
+
+private:
+    T value_a, value_b;
+};
+
+template <template <typename ...> typename Container, HasMul ItemT>
+using my_collection_iterator = use_collection_iterator<Container, my_item<ItemT>>;
+
+template <template <typename ...> typename Container, HasMul ItemT>
+class my_item_collection : public my_collection_iterator<Container, ItemT> {
+public:
+
+    // A convenient access to inherited type members
+    using base_types = my_collection_iterator<Container, ItemT>;
+
+    /* Custom collection operations */
+
+    void add_item(const ItemT &a, const ItemT &b) {
+        items.emplace_back(a, b);
+    }
+
+    typename base_types::iterator get_item(size_t idx) {
+        auto res = items.begin();
+        return std::advance(res, idx);
+    }
+
+    /* Required Iterators Access Methods */
+
+    typename base_types::iterator begin() { return items.begin(); }
+    typename base_types::iterator end() { return items.end(); }
+    [[nodiscard]] typename base_types::const_iterator cbegin() const { return items.cbegin(); }
+    [[nodiscard]] typename base_types::const_iterator cend() const { return items.cend(); }
+    typename base_types::reverse_iterator rbegin() { return items.rbegin(); }
+    typename base_types::reverse_iterator rend() { return items.rend(); }
+    [[nodiscard]] typename base_types::const_reverse_iterator crbegin() const { return items.crbegin(); }
+    [[nodiscard]] typename base_types::const_reverse_iterator crend() const { return items.crend(); }
+
+    [[nodiscard]] bool empty() const { return items.empty(); }
+    [[nodiscard]] typename base_types::size_type size() const { return items.size(); }
+
+private:
+    Container<my_item<ItemT>> items;
+};
+#endif
+
 int main() {
 #if ARTICLE_PART == 1
     std::list<int> i_list;
@@ -133,6 +316,25 @@ int main() {
 
     t.join();
     t1.join();
+#endif
+#if ARTICLE_PART == 3
+    my_item_collection<std::list, int> list_collection;
+    my_item_collection<std::vector, int> vector_collection;
+    list_collection.add_item(1, 2);
+    list_collection.add_item(3, 4);
+    list_collection.add_item(5, 6);
+
+    vector_collection.add_item(7, 8);
+    vector_collection.add_item(9, 10);
+    vector_collection.add_item(11, 12);
+
+    for (auto &elem : list_collection) {
+        std::cout << elem.item() << std::endl;
+    }
+
+    for (auto &elem : vector_collection) {
+        std::cout << elem.item() << std::endl;
+    }
 #endif
     return EXIT_SUCCESS;
 }
